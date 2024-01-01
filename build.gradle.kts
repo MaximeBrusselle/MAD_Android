0e07c4e9914b9724898675c51296d31f6c320128
maximebrusselle <maxime.brusselle@student.hogent.be>
2024-01-01 15:05:15 +0100

    add dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+
+    dependencies {
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
+    id("org.jetbrains.dokka") version "1.9.10"
 }
